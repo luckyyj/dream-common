98b3636009090ea72b33eda5be6bfdd72c5c5227
yangjun <173417092@qq.com>
2020-11-27 14:06:39 +0800

    redis工具类提交

--- src/main/java/cn/com/dream/common/redis/util/RedisUtil.java
+++ src/main/java/cn/com/dream/common/redis/util/RedisUtil.java
@@ -1,11 +1,8 @@
 package cn.com.dream.common.redis.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.DataType;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.Cursor;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
